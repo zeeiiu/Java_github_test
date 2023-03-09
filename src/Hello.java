5b948211fc5654520915bf9cfa60760cff5cc5e4
USER <USER@DESKTOP-NOPF6NQ>
2023-03-09 11:51:30 +0900

    1

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+		System.out.println("Hello");
+		
+// 		System.out.println("Hello");
+		System.out.println("Hello");
+
+	}
+
+}
+https://github.com/zeeiiu/Java_github_test/blob/master/src/Hello.java
