c98b6eed82c23d1beb1dc20087086ab64f5bcf07
zeeiiu <121531670+zeeiiu@users.noreply.github.com>
2023-03-09 11:45:32 +0900

    주석처리

--- src/Hello.java
+++ src/Hello.java
@@ -3,8 +3,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Hello");
+// 		System.out.println("Hello");
 		System.out.println("Hello");
 	}
 
 }
+https://github.com/zeeiiu/Java_github_test/blob/master/src/Hello.java
