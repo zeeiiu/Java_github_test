c0c78e51966fd2ae0a0f73ae2250e9196507583a
zeeiiu <121531670+zeeiiu@users.noreply.github.com>
2023-03-09 12:21:58 +0900

    수정2

--- src/Hello.java
+++ src/Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 
-		System.out.println("�ȳ�");
-		System.out.println("�߰�");
+	//	System.out.println("¾È³ç");
+		System.out.println("Àß°¡");
 		
 // 		System.out.println("Hello");
 	
